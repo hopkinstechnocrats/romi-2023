1777e5bbd811257559bc53aeb23b3771431d54e2
AJ Amelse <aj.amelse@hopkinsrobotics.org>
2023-01-12 20:22:38 -0600

    Added AutonomousAngle and DriveAngle

---
+++ romi balance/src/main/java/frc/robot/commands/DriveAngle.java
@@ -0,0 +1,58 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.subsystems.Drivetrain;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class DriveAngle extends CommandBase {
+  private final Drivetrain m_drive;
+  private final double m_angle;
+  private final double m_speed;
+
+  /**
+   * Creates a new DriveDistance. This command will drive your your robot for a desired distance at
+   * a desired speed.
+   *
+   * @param speed The speed at which the robot will drive
+   * @param inches The number of inches the robot will drive
+   * @param drive The drivetrain subsystem on which this command will run
+   */
+  public DriveAngle(double speed, double degrees, Drivetrain drive) {
+    m_angle = degrees;
+    m_speed = speed;
+    m_drive = drive;
+    addRequirements(drive);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_drive.arcadeDrive(0, 0);
+    m_drive.resetEncoders();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_drive.arcadeDrive(m_speed, 0);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    m_drive.arcadeDrive(0, 0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    double ax = m_drive.getAccelX();
+    double az = m_drive.getAccelZ();
+    double angle = Math.atan2(ax, az);
+    double angle_degrees = Math.toDegrees(angle);
+    return Math.abs(angle_degrees) >= m_angle;
+  }
+}
